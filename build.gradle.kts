95d70879b0065d08aa6dcc068baacf7a9bd8548e
booky10 <boooky10@gmail.com>
2022-05-08 05:11:42 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+}
+
+group = "dev.booky"
+version = "1.0.0"
+
+repositories {
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    api("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+}
+
+tasks.processResources {
+    inputs.property("version", version)
+    filesMatching("plugin.yml") {
+        expand("version" to version)
+    }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        artifactId = project.name.toLowerCase()
+        from(components["java"])
+    }
+}
