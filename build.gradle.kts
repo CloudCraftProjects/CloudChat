e2212ee33740ab0c2d49d63c23c0799841fa032d
booky10 <boooky10@gmail.com>
2024-07-04 17:37:39 +0200

    Bump dependencies to 1.20.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,19 @@ plugins {
     id("maven-publish")
 
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
-    id("xyz.jpenilla.run-paper") version "2.2.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("xyz.jpenilla.run-paper") version "2.3.0"
+    id("io.github.goooler.shadow") version "8.1.8"
 }
 
 group = "dev.booky"
 version = "1.1.3"
 
 repositories {
-    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
     compileOnlyApi("net.luckperms:api:5.4")
 
     implementation("org.bstats:bstats-bukkit:3.0.2")
@@ -24,8 +24,8 @@ dependencies {
 java {
     withSourcesJar()
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-        vendor.set(JvmVendorSpec.ADOPTIUM)
+        languageVersion = JavaLanguageVersion.of(21)
+        vendor = JvmVendorSpec.ADOPTIUM
     }
 }
 
@@ -45,12 +45,21 @@ bukkit {
 
 tasks {
     runServer {
-        minecraftVersion("1.20.2")
+        minecraftVersion("1.20.6")
 
         downloadPlugins {
             // bukkit plugin is not available on modrinth/hangar/github/etc.
-            url("https://download.luckperms.net/1519/bukkit/loader/LuckPerms-Bukkit-5.4.106.jar")
+            url("https://download.luckperms.net/1549/bukkit/loader/LuckPerms-Bukkit-5.4.134.jar")
         }
+    }
+
+    withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+
+    withType<Jar> {
+        // no spigot mappings are used, disable useless remapping step
+        manifest.attributes("paperweight-mappings-namespace" to "mojang")
     }
 
     shadowJar {
